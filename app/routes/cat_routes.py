0150f9cdc82c6ffd41a28ac2a8df6dc7b4d97767
Mikelle <mmikelle.wade@adadevelopersacademy.org>
2025-10-20 13:56:39 -0400

    Building an API 02: Read One and Errors

--- app/routes/cat_routes.py
+++ app/routes/cat_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import abort, Blueprint, make_response
 from app.models.cat import cats
 
 cats_bp = Blueprint("cat_bp", __name__, url_prefix="/cats")
@@ -16,3 +16,30 @@ def get_all_cats():
         ))
 
     return result_list
+
+@cats_bp.get("/<id>")
+def get_single_cat(id):
+    cat = validate_cat(id)
+    cat_dict = dict(
+        id=cat.id,
+        name=cat.name,
+        color=cat.color,
+        personality=cat.personality
+    )
+
+    return cat_dict
+
+def validate_cat(id):
+    try:
+        id = int(id)
+    except ValueError:
+        invalid = {"message": f"Cat id ({id}) is invalid."}
+
+        abort(make_response(invalid, 400))
+
+    for cat in cats:
+        if cat.id == id:
+            return cat
+    
+    not_found = {"messsage": f"Cat with id ({id}) not found."}
+    abort(make_response(not_found, 404))
